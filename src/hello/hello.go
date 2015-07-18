f45587e20381dd55c9b07d6233d25ec0b3808175
lookOnTheBrightSide <nkosindlovu87@gmail.com>
2015-07-18 21:13:04 +0200

    src

---
+++ src/hello/hello.go
@@ -0,0 +1,36 @@
+package main
+import "fmt"
+func main() {
+    fmt.Println("hello world")
+    fmt.Println("1 +  1 = ", 1+1)
+    fmt.Println(!true)
+    var e int
+    fmt.Println(e)
+    f := "short"
+    fmt.Println(f)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

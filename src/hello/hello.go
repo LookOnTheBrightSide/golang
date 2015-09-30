b8a2987d6da5ea93a05b52818267a5e6fda9f3f8
lookOnTheBrightSide <nkosindlovu87@gmail.com>
2015-09-30 21:12:12 +0200

    go functions

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -10,6 +10,35 @@ func main() {
     fmt.Println(f)
 }
 
+var x int = 5
+func f() {
+  fmt.Println(x)
+}
+  func main() {
+  f()
+}
+
+func f() (int, int) {
+  return 5, 6
+}
+
+func main() {
+  x, y := f()
+}
+
+func add(args ...int) int {
+  total := 0
+  for _, v := range args {
+    total += v
+  }
+  return total
+}
+func main() {
+  fmt.Println(add(1,2,3))
+}
+
+
+
 
 
 
